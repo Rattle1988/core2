9878e6160b381062a5d44a9c068c0e0bf6bd3266
rattle <paulosminog@gmail.com>
2022-10-01 01:14:01 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+C:\Java\level_2\home_work_6
